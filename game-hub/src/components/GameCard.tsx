e00b1b5fe1216d5c674f06f201482d547d4bcfcd
tyler1444186 <tlrwelsh@gmail.com>
2023-11-07 17:21:27 -0600

    Displaying Crititc Score

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
 import PlatFormIconList from './PlatFormIconList';
+import CriticScore from './CriticScore';
 
 interface Props {
   game: Game;
@@ -12,9 +13,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
-        <PlatFormIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent='space-between' overflow={'hiden'}>
+          <PlatFormIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
